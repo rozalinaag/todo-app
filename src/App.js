a00dd6c476a2037f9a51298f0fa3314926f07e60
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2022-03-21 11:53:33 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import listSvg from './assets/img/list.svg'
 
 function App() {
   return (
-    <h1> 123 Kras</h1>
+    <div className="todo">
+      <div className='todo__sidebar'>
+        <ul className='todo__list'>
+          <li>
+              <i>
+                <img src={listSvg} alt="List icon"></img>
+              </i>
+              <span>Все задачи</span>
+          </li>
+        </ul>
+      </div>
+      <div className='todo__tasks'>
+
+      </div>
+    </div>
   );
 }
 
